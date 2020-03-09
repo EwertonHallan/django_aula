345c5b2dee1d9813ce612eba2d0816405a19cae7
ewerton hallan <ewerton.hallabn@Suportes-iMac.local>
2020-03-09 09:22:41 -0300

    comandos para banco de dados (migrate)

--- my_project/migrate.sh
+++ my_project/migrate.sh
@@ -4,4 +4,16 @@ python manage.py makemigrations
 
 python manage.py migrate
 
+#comandos do banco:
+   # lista os comandos sql contidos no arquivo migrate
+   # python manage.py sqlmigrate app_name migrate_file_name
+   # gera comando para limpar a base de dados (delete's)
+   # python manage.oy selflush
+   # faz uma carga no bando de dados com base no arquivo json
+   # python manage.py loaddata file_name_json --app app_name -i
+   # faz uma exportacao dos dados para arquivo json
+   # python manage.py dumpdata [app_name[.model_name] ...] --output file_name.json 
+
+
+#fonte: https://docs.djangoproject.com/pt-br/1.11/ref/django-admin/#django-admin-sqlmigrate
 
